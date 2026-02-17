546e4fb5ba4ae0bc0e7c8bdcde67ab2f2d111cf6
Roman Tyler <romantyler01@gmail.com>
2026-02-16 20:20:39 -0700

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Roman";
+            int age = 34;
+            char initial = 'R';
+            double avg = 98.7;
+            decimal bankAccount = 66.6m;
+            bool single = true;
+            
+            Console.WriteLine($"Name: {myName}\nAge: {age}\nInitial: {initial}\nAverage: {avg}\nMoney: {bankAccount}\nSingle: {single}");
         }
     }
 }
